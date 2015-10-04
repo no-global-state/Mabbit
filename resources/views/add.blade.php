d1e554d057c0c0f9ac5060d29219bdcfc2df6770
David Yang <daworld.ny@gmail.com>
2015-10-04 07:27:44 +0500

    Replaced with partials

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -4,31 +4,9 @@
   <h2 class="page-header">Add new issue</h2>
   
   {!! Form::open(['action' => 'Issue@addAction']) !!}
-
-	<div class="form-group">
-	  {!! Form::label('name', 'Name:') !!}
-	  {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Enter the title of the issue']) !!}
-	</div>
-
-	<div class="form-group">
-	  {!! Form::label('description', 'Description:') !!}
-	  {!! Form::textarea('description', null, ['class' => 'form-control', 'placeholder' => 'Describe the issue constructively']) !!}
-	</div>
-
-	<div class="form-group">
-	  {!! Form::submit('Submit', ['class' => 'btn btn-primary']) !!}
-	</div>
-
+  	@include('partials.form')
   {!! Form::close() !!}
 
-   @if ($errors->any())
-
-   	<ul class="alert alert-danger">
-
-   		@foreach ($errors->all() as $error)
-   		<li>{{ $error }}</li>
-   		@endforeach
-
-   	</ul>
-   @endif
+   @include('partials.errors')
+   
 @stop
