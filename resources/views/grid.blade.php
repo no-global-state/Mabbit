31d7ae00eb9e236be9956019f07f25f77bc9fb10
David Yang <daworld.ny@gmail.com>
2015-10-12 16:25:08 +0500

    Improved grid's template

--- resources/views/grid.blade.php
+++ resources/views/grid.blade.php
@@ -13,22 +13,22 @@ Issues
 
 @if (!empty($records))
 
-<table class="table">
+<table class="table table-hover table-bordered table-striped table-condensed">
 	<thead>
-		<th class="text-muted">#</th>
+		<th class="text-center text-muted">#</th>
 		<th>Issue</th>
 		<th>Description</th>
-		<th>Solved</th>
-		<th>Actions</th>
+		<th class="text-center">Solved</th>
+		<th class="text-center">Actions</th>
 	</thead>
 	<tbody>
 		@foreach ($records as $record)
 		<tr>
-			<td class="text-muted">{{ $record->id }}</td>
+			<td class="text-center text-muted">{{ $record->id }}</td>
 			<td>{{ $record->name }}</td>
 			<td>{{ $record->description }}</td>
-			<td>{{ $record->solved ? 'Yes' : 'No' }}</td>
-			<td>
+			<td class="text-center">{{ $record->solved ? 'Yes' : 'No' }}</td>
+			<td class="text-center">
 				<a href="#"><i class="glyphicon glyphicon-eye-open"></i> </a>
 				<a href="{{ action('Issues@editViewAction', ['id' => $record->id]) }}"><i class="glyphicon glyphicon-file"></i> </a>
 				<a href="#"><i class="glyphicon glyphicon-remove"></i> </a>
