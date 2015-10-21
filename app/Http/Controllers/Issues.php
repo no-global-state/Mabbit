1f64e1e16580e83607d8eb5846a40036d47025a4
David Yang <daworld.ny@gmail.com>
2015-10-21 15:09:43 +0500

    Replaced tabs with whitespaces

--- app/Http/Controllers/Issues.php
+++ app/Http/Controllers/Issues.php
@@ -22,98 +22,98 @@ final class Issues extends BaseController
 
     /**
      * Displays a a grid with issues
-	 * 
-	 * @return string
-	 */
-	public function displayGridAction()
-	{
-		return view('grid', [
-			'records' => Issue::latest('id')->paginate(5)
-		]);
-	}
+     * 
+     * @return string
+     */
+    public function displayGridAction()
+    {
+        return view('grid', [
+            'records' => Issue::latest('id')->paginate(5)
+        ]);
+    }
 
-	/**
-	 * Displays an issue by its associated id
-	 * 
-	 * @param string $id Issue's id
-	 * @return string
-	 */
-	public function viewAction($id)
-	{
-		$issue = Issue::findOrFail($id);
+    /**
+     * Displays an issue by its associated id
+     * 
+     * @param string $id Issue's id
+     * @return string
+     */
+    public function viewAction($id)
+    {
+        $issue = Issue::findOrFail($id);
 
-		return view('view', [
-			'issue' => $issue
-		]);
-	}
+        return view('view', [
+            'issue' => $issue
+        ]);
+    }
 
-	/**
+    /**
 	 * Displays all issues
 	 * 
 	 * @return string
 	 */
-	public function addViewAction()
-	{
-		return view('add', [
-			'tags' => Tag::lists('name', 'id')
-		]);
-	}
+    public function addViewAction()
+    {
+        return view('add', [
+            'tags' => Tag::lists('name', 'id')
+        ]);
+    }
 
-	/**
-	 * Adds a new issue
-	 * 
-	 * @param \App\Http\Requests\IssueRequest $request
-	 * @return void
-	 */
-	public function addAction(IssueRequest $request)
-	{
-		// Add the issue itself
-		$model = Issue::create($request->all());
+    /**
+     * Adds a new issue
+     * 
+     * @param \App\Http\Requests\IssueRequest $request
+     * @return void
+     */
+    public function addAction(IssueRequest $request)
+    {
+        // Add the issue itself
+        $model = Issue::create($request->all());
 
-		// Add its tags
-		$model->tags()->attach($request->input('tag_list'));
+        // Add its tags
+        $model->tags()->attach($request->input('tag_list'));
 
-		\Session::flash('status', 'The issue has added updated successfully');
+        \Session::flash('status', 'The issue has added updated successfully');
 
-		return redirect()->action('Issues@displayGridAction');
-	}
+        return redirect()->action('Issues@displayGridAction');
+    }
 
-	/**
-	 * Displays issue's edit form
-	 * 
-	 * @param $id The issue id
-	 * @return string
-	 */
-	public function editViewAction($id)
-	{
-		$issue = Issue::findOrFail($id);
+    /**
+     * Displays issue's edit form
+     * 
+     * @param $id The issue id
+     * @return string
+     */
+    public function editViewAction($id)
+    {
+        $issue = Issue::findOrFail($id);
 
-		return view('edit', [
-			'model' => $issue,
-			'tags' => Tag::lists('name', 'id')
-		]);
-	}
+        return view('edit', [
+            'model' => $issue,
+            'tags' => Tag::lists('name', 'id')
+        ]);
+    }
 
-	/**
-	 * Updates an issue
-	 * 
-	 * @param App\Http\Requests\IssueRequest $request
-	 * @return void
-	 */
-	public function editAction(IssueRequest $request)
-	{
-		$input = $request->all();
+    /**
+     * Updates an issue
+     * 
+     * @param App\Http\Requests\IssueRequest $request
+     * @return void
+     */
+    public function editAction(IssueRequest $request)
+    {
+        $input = $request->all();
 
-		$model = Issue::findOrFail($input['id']);
-		$model->update($input);
+        $model = Issue::findOrFail($input['id']);
+        $model->update($input);
 
-		$tags = $request->input('tag_list', []);
+        $tags = $request->input('tag_list', []);
 
-		// Add its tags
-		$model->tags()->sync($tags);
+        // Add its tags
+        $model->tags()->sync($tags);
 
-		\Session::flash('status', 'The issue has been updated successfully');
+        \Session::flash('status', 'The issue has been updated successfully');
 
-		return redirect()->action('Issues@displayGridAction');
-	}
+        return redirect()->action('Issues@displayGridAction');
+    }
 }
