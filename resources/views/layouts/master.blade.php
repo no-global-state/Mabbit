16d242d35447c40b3bd486028284bd986894d79c
David Yang <daworld.ny@gmail.com>
2015-10-21 15:36:01 +0500

    Fixed default footer styles

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -37,8 +37,8 @@
 
     </div>
 
-    <footer class="blog-footer">
-      <p><a href="#">Back to top</a></p>
+    <footer>
+      <p>Copyright &copy; 2015 No Global State Lab</p>
     </footer>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
