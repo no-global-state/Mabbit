e22e16cf4761857bda5dca3d1bfb947bbc0e6da9
David Yang <daworld.ny@gmail.com>
2015-10-23 02:31:37 +0500

    Wrapped a grid into responsive container

--- resources/views/grid.blade.php
+++ resources/views/grid.blade.php
@@ -13,34 +13,34 @@ Issues
 
 @if (!empty($records))
 
-<table class="table table-hover table-bordered table-striped table-condensed">
-	<thead>
-		<th class="text-center text-muted">#</th>
-		<th>Issue</th>
-		<th>Created at</th>
-		<th>Updated at</th>
-		<th class="text-center">Solved</th>
-		<th class="text-center">Actions</th>
-	</thead>
-	<tbody>
-		@foreach ($records as $record)
-		<tr>
-			<td class="text-center text-muted">{{ $record->id }}</td>
-			<td>{{ $record->name }}</td>
-			<td>{{ $record->created_at }}</td>
-			<td>{{ $record->updated_at }}</td>
-			<td class="text-center">{{ $record->solved ? 'Yes' : 'No' }}</td>
-			<td class="text-center">
-				<a title="View description of this issue" href="{{ action('Issues@viewAction', ['id' => $record->id]) }}"><i class="glyphicon glyphicon-eye-open"></i> </a>
-				<a title="Edit this issue" href="{{ action('Issues@editViewAction', ['id' => $record->id]) }}"><i class="glyphicon glyphicon-file"></i> </a>
-				<a title="Remove this issue" href="#"><i class="glyphicon glyphicon-remove"></i> </a>
-			</td>
-		</tr>
-		@endforeach
-	</tbody>
-
-</table>
-
+<div class="table-responsive">
+    <table class="table table-hover table-bordered table-striped table-condensed">
+        <thead>
+            <th class="text-center text-muted">#</th>
+            <th>Issue</th>
+            <th>Created at</th>
+            <th>Updated at</th>
+            <th class="text-center">Solved</th>
+            <th class="text-center">Actions</th>
+        </thead>
+        <tbody>
+            @foreach ($records as $record)
+            <tr>
+                <td class="text-center text-muted">{{ $record->id }}</td>
+                <td>{{ $record->name }}</td>
+                <td>{{ $record->created_at }}</td>
+                <td>{{ $record->updated_at }}</td>
+                <td class="text-center">{{ $record->solved ? 'Yes' : 'No' }}</td>
+                <td class="text-center">
+                    <a title="View description of this issue" href="{{ action('Issues@viewAction', ['id' => $record->id]) }}"><i class="glyphicon glyphicon-eye-open"></i> </a>
+                    <a title="Edit this issue" href="{{ action('Issues@editViewAction', ['id' => $record->id]) }}"><i class="glyphicon glyphicon-file"></i> </a>
+                    <a title="Remove this issue" href="#"><i class="glyphicon glyphicon-remove"></i> </a>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 <div class="text-center">
 	{!! $records->render() !!}
 </div>
