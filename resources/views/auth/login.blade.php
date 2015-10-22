9b3260773a08129ea9339724d04b8c246bc864f5
David Yang <daworld.ny@gmail.com>
2015-10-23 02:13:33 +0500

    Forced login form to have a fixed width

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,30 +5,32 @@ Login
 @stop
 
 @section('content')
+<div class="col-lg-6">
 {!! Form::open(['url' => '/auth/login']) !!}
     
     <p class="text-muted"><i class="glyphicon glyphicon-user"></i> Don't have an account? <a href="/auth/register">Click here to create a new one</a></p>
 
-	<div class="form-group">
-	  {!! Form::label('email', 'Email:') !!}
-	  {!! Form::text('email', null, ['class' => 'form-control']) !!}
-	</div>
+    <div class="form-group">
+      {!! Form::label('email', 'Email:') !!}
+      {!! Form::text('email', null, ['class' => 'form-control']) !!}
+    </div>
 
-	<div class="form-group">
-	  {!! Form::label('password', 'Password:') !!}
-	  {!! Form::password('password', ['class' => 'form-control']) !!}
-	</div>
+    <div class="form-group">
+      {!! Form::label('password', 'Password:') !!}
+      {!! Form::password('password', ['class' => 'form-control']) !!}
+    </div>
 
-	<div class="form-group">
-	  {!! Form::label('remember', 'Remember me') !!}
-	  {!! Form::checkbox('remember') !!}
-	</div>
+    <div class="form-group">
+      {!! Form::label('remember', 'Remember me') !!}
+      {!! Form::checkbox('remember') !!}
+    </div>
 
     @include('partials.errors')
 
-	<div class="form-group">
-	  {!! Form::submit('Submit', ['class' => 'btn btn-primary']) !!}
-	</div>
+    <div class="form-group">
+      {!! Form::submit('Submit', ['class' => 'btn btn-primary']) !!}
+    </div>
 
 {!! Form::close() !!}
+</div>
 @stop
