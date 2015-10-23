e605603f8cbd138e1b597cb4dfd17772efa9a753
David Yang <daworld.ny@gmail.com>
2015-10-23 17:36:42 +0500

    Set up token for AJAX requests

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,31 @@
 
 $(function(){
 	$('select').select2();
+    
+    // Setup the shared token
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+    
+    $("a[data-issue-id]").click(function(event){
+        // Track the clicked id
+        var id = $(this).data('issue-id');
+        
+        // Attach a unique even handler for a button
+        $("button[data-issue-button='remove']").off('click').click(function(event){
+            $.ajax({
+                url : '/issues/remove.ajax',
+                type : 'POST',
+                data : {
+                    id : id
+                },
+                success : function(response){
+                    alert(response);
+                }
+            });
+        });
+    });
+    
 });
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-  	<title>@yield('title')</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <title>@yield('title')</title>
     
     <link href="/css/bootstrap.min.css" rel="stylesheet" />
     <link href="/css/styles.css" rel="stylesheet" />
