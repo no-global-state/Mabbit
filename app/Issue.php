c4a9ee512ef4d7225f5e38cf971d588702decfac
David Yang <daworld.ny@gmail.com>
2015-10-23 23:16:41 +0500

    Replaced tabs with whitespaces

--- app/Issue.php
+++ app/Issue.php
@@ -4,37 +4,37 @@ use Illuminate\Database\Eloquent\Model;
 
 class Issue extends Model
 {
-	/**
-	 * {@inheritDoc}
-	 */
-	protected $table = 'system_issues';
+    /**
+     * {@inheritDoc}
+     */
+    protected $table = 'system_issues';
+
+    /**
+     * {@inheritDoc}
+     */
+    protected $fillable = [
+        'name', 
+        'description',
+        'solved'
+    ];
 
 	/**
-	 * {@inheritDoc}
-	 */
-	protected $fillable = [
-		'name', 
-		'description',
-		'solved'
-	];
+     * Fetch all tags associated with a model
+     * 
+     * @return array
+     */
+    public function tags()
+    {
+        return $this->belongsToMany('App\Tag');
+    }
 
-	/**
-	 * Fetch all tags associated with a model
-	 * 
-	 * @return array
-	 */
-	public function tags()
-	{
-		return $this->belongsToMany('App\Tag');
-	}
-
-	/**
-	 * Implementation for tag_list magic property 
-	 * 
-	 * @return array
-	 */
-	public function getTagListAttribute()
-	{
-		return $this->tags()->lists('id')->toArray();
-	}
+    /**
+     * Implementation for tag_list magic property 
+     * 
+     * @return array
+     */
+    public function getTagListAttribute()
+    {
+        return $this->tags()->lists('id')->toArray();
+    }
 }
