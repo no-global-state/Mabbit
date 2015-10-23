98ee446bf1733ee710c5c13ad5869d9493ac42e4
David Yang <daworld.ny@gmail.com>
2015-10-23 17:52:12 +0500

    Added removeAction to routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,6 +18,7 @@ Route::post('/issues/add.ajax', 'Issues@addAction');
 Route::get('/issues/edit/{id}', 'Issues@editViewAction');
 Route::post('/issues/edit.ajax', 'Issues@editAction');
 Route::get('/issues/view/{id}', 'Issues@viewAction');
+Route::post('/issues/remove.ajax', 'Issues@removeAction');
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
