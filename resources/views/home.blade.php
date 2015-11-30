2e3bf93e7e9915a4022face215e4f3057d3d977f
David Yang <daworld.ny@gmail.com>
2015-11-30 10:55:27 +0500

    Improved home page looks

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@ Welcome to Mabbit
 @stop
 
 @section('content')
-<article>
+<article class="home">
     <p>Mabbit is a lightweight issue tracking system. It can be used to manage TODO lists or project bugs</p>
     <p>In order to start using it, you need to <a href="/auth/login">login</a> or <a href="/auth/register">register</a> a new account</p>
 </article>
